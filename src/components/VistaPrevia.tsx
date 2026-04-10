f26ec6533c80254537fc07285a31680a58d635d5
andreanicolecadimarodriguez-cr <andreanicolecadimarodriguez@gmail.com>
2026-04-09 22:49:28 -0400

    commit-vista-previa

---
+++ src/components/VistaPrevia.tsx
@@ -0,0 +1,11 @@
+export default function VistaPrevia() {
+  return (
+    <div>
+      <h2>Vista previa</h2>
+
+      <div className="preview-file">
+        <p>archivo.pdf</p>
+      </div>
+    </div>
+  );
+}
